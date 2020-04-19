8042d35181c0352454afc9d2a15eb91b02ca90f2
Anion_Zed <dianaakbasheva21@gmail.com>
2020-04-19 19:53:43 +0300

    attachment menu and all related

--- Assets/Scripts/Weapon Creation/WeaponCreationSystem.cs
+++ Assets/Scripts/Weapon Creation/WeaponCreationSystem.cs
@@ -71,11 +71,10 @@ public class WeaponCreationSystem : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.KeypadEnter))
-        {
-            //Test code
-            //GameObject weapon = CreateWeapon(selectedRodAttachment, selectedAttackTypeAttachment, selectedElementTypeAttachment);
-            //weapon.transform.position = new Vector3(-3,0,0);
+        if (Input.GetKeyDown(KeyCode.Q))
+        { 
+            GameObject weapon = CreateWeapon(selectedRodAttachment, selectedAttackTypeAttachment, selectedElementTypeAttachment);
+            weapon.transform.position = new Vector3(-3, 0, 0);
         }
     }
 }
