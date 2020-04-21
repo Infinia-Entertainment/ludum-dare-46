ae9c242a4484c7d7eb49e2bfdeea43a1c48322b0
RebornInferno <antonpirogov@mail.ru>
2020-04-21 03:13:13 +0300

    Revert "Revert "fixed loading issue, rewards apply properly""
    This reverts commit 272a0b52359a76d4a21ff2eed9b80f0cf4808dbb.

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class GameStateManager : MonoBehaviour
 {
@@ -46,9 +47,10 @@ public class GameStateManager : MonoBehaviour
         {
             WinStage(FindObjectOfType<WaveManager>().currentStage);
         }
+
     }
 
-    
+
 
     private void Awake()
     {
@@ -89,18 +91,34 @@ public class GameStateManager : MonoBehaviour
 
         MoveUnusedHeroes();
 
+
+
+        StartCoroutine(LoadBattleScene());
+
+        
+        //Lul because we're such good programmers lmao
         
 
-        SceneManager.LoadScene(1);
+        
 
-        //Lul because we're such good programmers lmao
+        
+
+
+        
+        //waveManager.currentStage = gameStages[currentStageIndex];
+    }
+
+    private IEnumerator LoadBattleScene()
+    {
+        // Start loading the scene
+        AsyncOperation asyncLoadLevel = SceneManager.LoadSceneAsync(1);
+        // Wait until the level finish loading
+        while (!asyncLoadLevel.isDone)
+            yield return null;
+        // Wait a frame so every Awake and Start method is called
+
         HideHeroes();
         HideWeapons();
-
-
-        InstantiateHeroUI();
-
-        
 
         for (int i = 0; i < currentHeroes.Count; i++)
         {
@@ -110,13 +128,30 @@ public class GameStateManager : MonoBehaviour
             heroController.InitializeWeaponPosition();
         }
 
+        InstantiateHeroUI();
+
 
         WaveManager waveManager = FindObjectOfType<WaveManager>();
+        Debug.Log(SceneManager.GetActiveScene().name);
 
-        //Debug.Log(waveManager.currentStage);
-        //waveManager.currentStage = gameStages[currentStageIndex];
+        Debug.Log(waveManager);
+        yield return new WaitForEndOfFrame();
     }
+    private IEnumerator LoadShopScene()
+    {
+        // Start loading the scene
+        AsyncOperation asyncLoadLevel = SceneManager.LoadSceneAsync(0);
+        // Wait until the level finish loading
+        while (!asyncLoadLevel.isDone)
+            yield return null;
+        // Wait a frame so every Awake and Start method is called
 
+        CalculateGold();
+
+        blacksmithCurrentHealth = blacksmithMaxHealth;
+
+        yield return new WaitForEndOfFrame();
+    }
     private void MoveUnusedHeroes()
     {
         int numberOfunusedHeroes = currentHeroes.Count - currentWeapons.Count;
@@ -160,11 +195,9 @@ public class GameStateManager : MonoBehaviour
         StoreHeores();
         DeleteWeaponData();
 
-        SceneManager.LoadScene(0);
+        StartCoroutine(LoadShopScene());
 
-        CalculateGold();
-
-        blacksmithCurrentHealth = blacksmithMaxHealth;
+       
     }
 
     public void WinStage(Stage wonStage)
